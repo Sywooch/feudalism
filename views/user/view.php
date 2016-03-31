996966f6272b3c36ca0675009d03cd7d49cd721c
Ilya Gorokhov <tairesh.rus@gmail.com>
2016-03-31 11:19:47 +0500

    html fix

--- views/user/view.php
+++ views/user/view.php
@@ -10,7 +10,9 @@ use yii\helpers\Html,
 $this->title = Yii::t('app','Feudalism') . ' — ' . $model->fullName;
 
 ?>
-<h1><?=$model->fullName?></h1>
-<div class="progress" title="<?=Yii::t('app', '{0,number} XP / {1,number} for level {2}',[$model->experience, ExperienceCalculator::getExperienceByLevel($model->level+1), $model->level+1])?>" >
-    <div class="progress-bar" style="width: <?=round(ExperienceCalculator::getPercentOfNextLevel($model->experience))?>%;"></div>
+<div class="col-lg-6 col-md-6">
+    <h1><?=$model->fullName?></h1>
+    <div class="progress" title="<?=Yii::t('app', '{0,number} XP / {1,number} for level {2}',[$model->experience, ExperienceCalculator::getExperienceByLevel($model->level+1), $model->level+1])?>" >
+        <div class="progress-bar" style="width: <?=round(ExperienceCalculator::getPercentOfNextLevel($model->experience))?>%;"></div>
+    </div>
 </div>
